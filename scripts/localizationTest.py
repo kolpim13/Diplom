671e9516addef7c0127a3c3a0352239b9770edb5
kolpim13 <torgunakovmaks2@gmail.com>
2022-08-24 14:29:26 +0300

    upd

--- scripts/localizationTest.py
+++ scripts/localizationTest.py
@@ -14,6 +14,23 @@ from sensor_msgs.msg import LaserScan
 import wallTest
 #=======================================================================================#
 
+def create_model(pub: rospy.Publisher, name: str, reference: str) -> ModelState:
+    model = ModelState()
+
+    model.model_name = name
+    model.reference_frame = reference
+
+    return model
+
+laser_topic_name = "/laser/scan"
+
+def laser_topic_read():
+    laser = rospy.wait_for_message(laser_topic_name, LaserScan)
+    distances = [round(item, 5) for item in laser.ranges]
+
+    return distances
+#=======================================================================================#
+
 # PRE PROCESSING
 def read_descriptor():
     with open(PATH_TO_MAP_DESCRIPTOR, 'r') as jsonfile:
@@ -39,51 +56,63 @@ def read_descriptor():
             k)
 
 """ 
-Compress raw data from file and write it into the RAM as map. 
-Every map contains of 3 elements: real localization, position and orient histograms
+Compress raw data from file and write it into the RAM as map [4 different arrays]. 
+Every map contains of 4 elements: real localization, position and orient histograms, feasibility of the point
 map - path to file with map
-lines - total amiunt of lines in this file 
+records - total amount of lines/points/positions in this map-file
+range_min_max - minimal and maximum laser distance
+hist_limit_pos_orient - amount of sectors for every type of histogram
+
+return (localization:[][], position:[][], orientation:[][], feasibility:bool
 """
-def buffer_map(map: str, lines: int, 
-                laser_count: int, range_min: np.float32, range_max: np.float32,
-                hist_limit_pos: int, hist_limit_orient: int):
-
-    # 2D arrays with position, orientation histograms and real positions
-    map_pos = np.zeros((lines, hist_limit_pos), dtype=np.int32)
-    map_orient = np.zeros((lines, hist_limit_orient), dtype=np.float32)
-    map_loc = np.zeros((lines, 3), dtype=np.float32)
-    map_existence = np.zeros((lines), dtype=Boolean)
-
+def buffer_map(map: str, records: int, 
+            range_min: np.float32, range_max: np.float32,
+            hist_limit_pos: int, hist_limit_orient: int) -> tuple:
+    
+    # Arrays that are represent map
+    map_pos = np.zeros((records, hist_limit_pos), dtype=np.int32)
+    map_orient = np.zeros((records, hist_limit_orient), dtype=np.float32)
+    map_loc = np.zeros((records, 3), dtype=np.float32)
+    map_feas = np.zeros((records), dtype=Boolean)
+    
     i = 0
     j = 0
     with open(map, 'r') as jsonfile:
-        for index in range(int(lines)):
+        for index in range(records):
+            # Read data from json
             line = jsonfile.readline()
             json_line = json.loads(line)
             raw_data = json_line["raw_data"]
 
+            # Fill arrays
+                # With zero data if raw data is None
             if raw_data == None:
-                map_pos[index, :] = np.zeros((60))
-                map_orient[index, :] = np.zeros((60))
+                map_pos[index, :] = np.zeros((hist_limit_pos))
+                map_orient[index, :] = np.zeros((hist_limit_orient))
                 map_loc[index, :] = np.array(json_line["pos"], dtype=np.float32)
-                map_existence[index] = False
+                map_feas[i] = False
+                # With histograms if something is present
             else:
                 data = raw_data_preprocessing(raw_data, range_min, range_max)
-                map_pos[index, :] = compress_pos(data, hist_limit_pos, range_max)
-                map_orient[index, :] = compress_orient(data, hist_limit_orient, laser_count)
+                map_pos[index, :] = compress_pos(data, range_max, hist_limit_pos)
+                map_orient[index, :] = compress_orient(data, laser_count, hist_limit_orient)
                 map_loc[index, :] = np.array(json_line["pos"], dtype=np.float32)
-                map_existence[index] = True
+                map_feas[i] = True
 
+            # For debugging purpose
             i += 1
             if i == 1000:
                 i = 0
                 j += 1
-                rospy.loginfo("Mapping %.2f procent", (j * 1000 / lines))
+                rospy.loginfo("Mapping %.2f procent", (j * 1000 / records))
 
-    return (map_loc, map_pos, map_orient, map_existence)
+    return (map_loc, map_pos, map_orient, map_feas)
 
 """
-Modify raw laser scans for further working process 
+Modify raw laser scans for further working process
+data:[] - one dimensional array with raw laser data
+
+return data prepeared for the compression into histograms
 """
 def raw_data_preprocessing(data, range_min: np.float32, range_max: np.float32):
     for i in range(len(data)):
@@ -96,6 +125,7 @@ def raw_data_preprocessing(data, range_min: np.float32, range_max: np.float32):
 
     return data
 #=======================================================================================#
+
 def laser_get_parameters():
     laser_topic_name = "/laser/scan"
     laser = rospy.wait_for_message(laser_topic_name, LaserScan)
@@ -103,15 +133,15 @@ def laser_get_parameters():
 #=======================================================================================#
 
 # HISTOGRAMS
+
 """
-data - prepeared(withount inf and -inf) raw laser data
-range_max - laser maximum range
-hist_limit - amount of bars in histogram
-laser_count - amount of laser scans
+Compress prepeared raw laser data into positional histogram
+
+return position histogram as numpy 1D array
 """
-def compress_pos(data, hist_limit: int, range_max: np.float32):
-    range = range_max / hist_limit
+def compress_pos(data, range_max: np.float32, hist_limit: int):
     hist = np.zeros(hist_limit)
+    range = range_max / hist_limit
 
     for item in data:
         lim = math.floor(item / range)
@@ -119,7 +149,12 @@ def compress_pos(data, hist_limit: int, range_max: np.float32):
 
     return hist
 
-def compress_orient(data, hist_limit: int, laser_count: int):
+"""
+Compress prepeared raw laser data into priental histogram
+
+return orient histogram as numpy 1D array
+"""
+def compress_orient(data, laser_count: int, hist_limit: int):
     hist = np.zeros(hist_limit)
     ranges_in_sector = int(laser_count / hist_limit)
     
@@ -133,8 +168,14 @@ def compress_orient(data, hist_limit: int, laser_count: int):
 
     return hist
 
-def match_pos(hist1, hist2, laser_limit):
-    amount = laser_limit * 2
+"""
+hist1, hist2 - positional histograms to be compared. Given as numpy 1D array
+laser_count - amount of lasers
+
+return error(read differents) in two histograms
+"""
+def match_pos(hist1, hist2, laser_count: int):
+    amount = laser_count * 2
 
     sum = 0
     for i in range(len(hist1)):
@@ -147,36 +188,48 @@ def match_pos(hist1, hist2, laser_limit):
 """
 Function for linear searching through all histograms.
 hist - histogram I want to compare 
-map_pos - 2d numpy array where all position histograms are listed  
-count - amount of histograms in the array
-"""
-def find_pos(hist, map_pos, laser_limit: int):
-    min_error: float = 100.0
-    best_index: int = 0
+map_pos - 2d numpy array where all position histograms are listed
+map_feas - 1D boolean array with info if particular point exist 
+records - amount of histograms in the array
 
-    for i in range(len(map_pos)):
-        if map_pos[i] == None:
+return index of the best candidate on this map
+"""
+def find_pos(hist, map_pos, map_feas, records, laser_count: int) -> int:
+    min_error: float = 100.0
+    index: int = 0
+
+    for i in range(records):
+        if map_feas[i] == False:
             continue
 
-        error = match_pos(hist, map_pos[i], laser_limit)
+        error = match_pos(hist, map_pos[i], laser_count)
         if min_error > error:
             min_error = error
-            best_index = i
+            index = i
 
-    return best_index
+    return index
 
-def find_pos_on_region(hist, map_pos, region, amount, laser_limit: int):
+"""
+Function search for the best candidate on specific region.
+hist - histogram I want to compare 
+map_pos - 2d numpy array where all position histograms are listed 
+map_feas - 1D boolean array with info if particular point exist
+records - amount of histograms in the array
+region - 1D array of indecses that shows specific map positions I need to look on.
+
+return index of the best candidate on the given map region
+"""
+def find_pos_on_region(hist, map_pos, map_feas, records: int, region, laser_count: int) -> int:
     min_error: float = 100.0
     best_index: int = 0
-
+    
     for i in range(len(region)):
-        if region[i] < 0 or region[i] >= amount:
+        if map_feas[i] == False:
+            continue
+        if region[i] < 0 or region[i] >= records:
             continue
 
-        if map_pos[region[i]] == None:
-            continue
-
-        error = match_pos(hist, map_pos[region[i]], laser_limit)
+        error = match_pos(hist, map_pos[region[i]], laser_count)
         if min_error > error:
             min_error = error
             best_index = region[i]
@@ -184,29 +237,38 @@ def find_pos_on_region(hist, map_pos, region, amount, laser_limit: int):
     return best_index
 
 """
-Function for calculation index of the candidate on one map with given index from smaller map
+Searching for position on a map with higher resolution that is corresponds with given indeks of a map with the smaller resolution
+mapn_x/y - maximum records of one axis.
 
-return index on the more precise map
+return map1_index of the corresponding position
 """
-def find_corresponding_pos(lowRes_index: int, lowRes_amount_x: int, lowRes_amount_y: int,
-                            highRes_amount_x: int, highRes_amount_y: int,
-                            k: float) -> int:
+def find_corresponding_point(map1_index: int, k: float, map1_x: int, map1_y: int, map2_x: int, map2_y: int) -> int:
+    x_1 = int(map1_index % map1_x)
+    y_1 = int(map1_index / map1_y)
+    x_2 = int(x_1 * k)
+    y_2 = int(y_1 * k)
+    map2_index = y_2 * map2_y + x_2
 
-    x_low = int(lowRes_index % lowRes_amount_x)
-    y_low = int(lowRes_index / lowRes_amount_y)
-    y_high = int(y_low * k)
-    x_high = int(x_low * k)
-    highRes_index = y_high * highRes_amount_y + x_high
+    return map2_index
 
-    return highRes_index
+"""
+Find all points that are around given indexed point inside some radius
+map_index - index 
+radius - half side of a square
 
-def find_region_to_search(index: int, k):
-    region = np.zeros(121, dtype=np.int32)
+return 1D np array with indexes
+"""
+def find_region_to_search(map_index: int, radius: int, max_x: int, map_y: int):
+    min = -radius
+    max = radius + 1
+    total = (max - min)**2
+
+    region = np.zeros(total, dtype=np.int32)
 
     index = 0
-    for i in range(-5, 5+1):
-        for j in range(-5, 5+1):
-            region[index] = np.int32(round(index + highRes_amount_y * i + j))
+    for i in range(min, max):
+        for j in range(min, max):
+            region[index] = np.int32(round(map_index + map_y * i + j))
             index += 1
 
     return region
@@ -224,8 +286,8 @@ if __name__ == '__main__':
     map_name = rospy.get_param("~map_name")
     model_name = rospy.get_param("~gazebo_model_name")
 
-    hist_pos_limit = rospy.get_param("~hist_pos_limit")
-    hist_orient_limit = rospy.get_param("~hist_pos_limit")
+    hist_limit_pos = rospy.get_param("~hist_pos_limit")
+    hist_limit_orient = rospy.get_param("~hist_pos_limit")
 
     laser_count = rospy.get_param("~laser_count")
 
@@ -244,51 +306,59 @@ if __name__ == '__main__':
      highRes, highRes_amount_x, highRes_amount_y, highRes_amount_total,
      k) = read_descriptor()
 
-    # Buffer maps
+    # Buffer maps [Make maps from raw data in the file]
     rospy.loginfo("Buffer map low res")
-    (map_loc_low, map_pos_low, map_orient_low) = buffer_map(PATH_TO_MAP_LOW_RES, lowRes_amount_total, 
-                                                            laser_count, range_min, range_max, 
-                                                            hist_pos_limit, hist_orient_limit)
+    (map_loc_low, map_pos_low, map_orient_low, map_feas_low) = buffer_map(PATH_TO_MAP_LOW_RES, lowRes_amount_total, 
+                                                            range_min, range_max, hist_limit_pos, hist_limit_orient)
     rospy.loginfo("Buffer map high res")
-    (map_loc_high, map_pos_high, map_orient_high) = buffer_map(PATH_TO_MAP_HIGH_RES, highRes_amount_total, 
-                                                            laser_count, range_min, range_max, 
-                                                            hist_pos_limit, hist_orient_limit)
+    (map_loc_high, map_pos_high, map_orient_high, map_feas_high) = buffer_map(PATH_TO_MAP_HIGH_RES, highRes_amount_total, 
+                                                            range_min, range_max, hist_limit_pos, hist_limit_orient)
     rospy.loginfo("Buffer maps from files done")
     rospy.loginfo("================================================================")
 
     # Publishers and subscribers (from wallTest)
     pub_model = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size = 10)
-    model = wallTest.create_model(pub_model, name=model_name, reference="")
-
-    # Set robot start position (from wallTest)
-    wallTest.model_move_init_pose(model, pub_model, 0, 0)
+    model = create_model(pub_model, name=model_name, reference="")
 
     # Main loop
     while not rospy.is_shutdown():
         start_time = time.time()
 
-        ranges = wallTest.laser_topic_read()
-        ranges = raw_data_preprocessing(ranges, range_min, range_max)
-        hist_pos = compress_pos(ranges, hist_pos_limit, range_max)
-        hist_orient = compress_orient(ranges, hist_orient_limit, laser_count)
-        rospy.loginfo("Raw data was read and compressed into hist_posograms")
+        # Read and process raw laser data
+        rospy.loginfo("Read raw data: ...")
+        raw_data = laser_topic_read() #probably
+        data = raw_data_preprocessing(raw_data, range_min, range_max)
+        hist_pos_current = compress_pos(data, range_max, hist_limit_pos)
+        hist_orient_current = compress_orient(data, laser_count, hist_limit_orient)
+        rospy.loginfo("Raw data was read and compressed into histograms")
 
         # Find best candidate on the low resolution map
-        best_index_low = find_pos(hist_pos, map_pos_low, laser_count)
-        best_low = np.array(map_loc_low[best_index_low])
-        rospy.loginfo("Best candidate on the low resolution map:")
-        rospy.loginfo("x_low = %.5f, y_high = %.5f", best_low[0], best_low[1])
+        best_index_low = find_pos(hist_pos_current, map_pos_low, map_feas_low, lowRes_amount_total, laser_count)
+        best_low = map_loc_low[best_index_low]
+        
+        # Find position on the high resolution map that is corelate to low res map 
+        highRes_corresponding_index = find_corresponding_point(best_index_low, k, 
+                                                                lowRes_amount_x, lowRes_amount_y,
+                                                                highRes_amount_x, highRes_amount_y)
+        highRes_region_to_search = find_region_to_search(highRes_corresponding_index, 5,
+                                                        highRes_amount_x, highRes_amount_y)
 
-        # Find nearest points on the high resolution map according to the best low resolution candidate
-        highRes_corresponding_index = find_corresponding_pos(best_index_low)
-        region_to_search = find_region_to_search(highRes_corresponding_index, k)
-        best_index_high = find_pos_on_region(hist_pos, map_pos_high, region_to_search, highRes_amount_total, laser_count)
-        best_high = np.array(map_loc_high[best_index_high])
-        rospy.loginfo("Best candidate on the high resolution map:")
+        best_index_high = find_pos_on_region(hist_pos_current, map_pos_high, map_feas_high,
+                                            highRes_amount_total, highRes_region_to_search, laser_count)
+        best_high = map_loc_high[best_index_high]
+
+        # Approximated position
+
+
+        # Orientation calculating    
+            
+
+        # Print all found data for debugging purposes
+        rospy.loginfo("Best candidate on low resolution map:")
+        rospy.loginfo("x_low = %.5f, y_low = %.5f", best_low[0], best_low[1])
+        rospy.loginfo("Best candidate on high resolution map:")
         rospy.loginfo("x_high = %.5f, y_high = %.5f", best_high[0], best_high[1])
-
-
-
+    
         rospy.loginfo("This step took %.3f", (time.time() - start_time))
         rospy.loginfo("================================================================")
         rate.sleep()
